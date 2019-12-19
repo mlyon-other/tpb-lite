26e7bacfcc1a86b703e92373b37ea7debdde8b34
Matt Lyon <matthewlyon18@gmail.com>
2019-12-19 10:20:10 +0000

    minor fix to description on setup

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from os import path
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '0.2.3'
 
 setup(name = 'tpblite',
-        version = '0.2.2',
+        version = version,
         description = 'The Unofficial Pirate Bay Lightweight Python API',
         author = 'Matt Lyon',
         author_email = 'matthewlyon18@gmail.com',
         python_requires = '>=3.6',
         url = 'https://github.com/mattlyon93/tpb-lite',
-        download_url = 'https://github.com/mattlyon93/tpb-lite/archive/v0.2.2.tar.gz',
+        download_url = 'https://github.com/mattlyon93/tpb-lite/archive/v{}.tar.gz'.format(version),
+        long_description = long_description,
+        long_description_content_type='text/markdown',
         license = 'MIT License',
         packages = ['tpblite', 'tpblite/models'],
         install_requires = [
